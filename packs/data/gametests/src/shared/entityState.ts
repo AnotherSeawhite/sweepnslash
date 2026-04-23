9023f4572850cd2882e96e6435c4680dcf1d196f
theaddon <theaddonn@gmail.com>
2026-04-23 04:48:28 +0200

    refactor: add entityState module for typed entity Maps

---
+++ packs/data/gametests/src/shared/entityState.ts
@@ -0,0 +1,3 @@
+export const playerHitMap = new Map<string, boolean>();
+export const lastAttackMap = new Map<string, { rawDamage: number; damage: number; time: number }>();
+export const rawDamageMap = new Map<string, number>();
