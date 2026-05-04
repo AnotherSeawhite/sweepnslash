7e96e6d9b3cc0fa34795cc6c5b10d274cd43ca76
theaddon <theaddonn@gmail.com>
2026-05-04 06:40:55 +0200

    fix: dimension weirdo

--- packs/data/gametests/src/handlers/startup.ts
+++ packs/data/gametests/src/handlers/startup.ts
@@ -82,7 +82,7 @@ export function registerStartupHandlers(): void {
     });
 
     world.afterEvents.weatherChange.subscribe(({ dimension, newWeather }) => {
-        const type = DimensionTypes.get(dimension)!;
+        const type = DimensionTypes.get(dimension.toLowerCase())!;
         world.setDynamicProperty(`sns:weather:${type.typeId}`, newWeather);
     });
 }
